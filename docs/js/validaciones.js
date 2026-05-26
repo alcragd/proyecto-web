a5d7ad47b961b0c3f7b974d82a28e99ba9e3c5bd
alcra <65140768+alcragd@users.noreply.github.com>
2026-05-26 08:40:55 -0600

    curp

--- docs/js/validaciones.js
+++ docs/js/validaciones.js
@@ -2,7 +2,7 @@ const regexBoleta = /^(?:\d{10}|P[PE]\d{8})$/;
 const regexNombre = /^(?:[A-Za-zÁÉÍÓÚÜÑáéíóúüñ]+(?:\s+[A-Za-zÁÉÍÓÚÜÑáéíóúüñ]+)*)$/;
 const regexTelefono = /^\d{10}$/;
 const regexCorreo = /^(?!\.)(?!.*\.\.)[A-Za-z0-9!#$%&'*+/=?^_`{|}~]+(?:\.[A-Za-z0-9!#$%&'*+/=?^_`{|}~]+)*@alumno\.ipn\.mx$/;
-const regexCurp = /^[A-ZÁÉÍÓÚÑ]{4}\d{6}[HM][A-ZÁÉÍÓÚÑ]{5}[A-Z0-9]{2}$/;
+const regexCurp = /^[A-ZÁÉÍÓÚÑ]{4}\d{6}[HM][A-ZÁÉÍÓÚÑ]{5}[A-Z0-9][0-9]$/;
 const regexContra = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{6,}$/;
 const regexFechaNacimiento = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
 const regexPromedio = /^(?:[6-9](?:\.\d{1,2})?|10(?:\.0{1,2})?)$/;
